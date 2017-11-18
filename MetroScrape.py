3bd04580dcc9739e46ea22fe928f3801f3b46da3
CJW <cwal37@gmail.com>
2017-11-17 21:15:10 -0500

    Setting up the date loop
    Have to get the data 31 days at a time.

--- MetroScrape.py
+++ MetroScrape.py
@@ -6,15 +6,29 @@ Created on Fri Nov 17 19:16:44 2017
 """
 
 import pandas as pd
-import pdb
-import time
+import pdb, time, datetime
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 keyValues = open('keyValues.txt')
 keyValues = keyValues.read().split(',')
 
-pdb.set_trace()
+#pdb.set_trace()
+
+"""
+This data begins on October 15th, so we want to generate date pairs from then
+until whenever now is.
+"""
+
+datePairs = []
+i = 0
+dateStart = datetime.datetime.strptime('10/1/2015', '%m/%d/%Y')
+while dateStart <= datetime.datetime.now():
+    print(dateStart)
+    datePairs.append([dateStart.strftime('%m/%d/%Y'), (dateStart+datetime.timedelta(days=30)).strftime('%m/%d/%Y')])
+    dateStart = dateStart + datetime.timedelta(days=31)
+    
+#pdb.set_trace()
 
 chromeOptions = webdriver.ChromeOptions()
 preferences = {"download.default_directory" : keyValues[0]}
@@ -49,12 +63,20 @@ time.sleep(1)
 driver.find_element_by_xpath('//*[@id="ctl00_ctl00_MainContent_MainContent_rbByRange"]').click()
 time.sleep(1)
 
-driver.find_element_by_xpath('//*[@id="ctl00_ctl00_MainContent_MainContent_txtStartDate"]').send_keys('11/1/2017')
-driver.find_element_by_xpath('//*[@id="ctl00_ctl00_MainContent_MainContent_txtEndDate"]').send_keys('11/30/2017')
-time.sleep(1)
-driver.find_element_by_xpath('//*[@id="ctl00_ctl00_MainContent_MainContent_btnSubmit"]').send_keys(Keys.RETURN)
-time.sleep(1)
-driver.find_element_by_xpath('//*[@id="ctl00_ctl00_MainContent_MainContent_lnkExport"]').send_keys(Keys.RETURN)
+i = 0
+for datePair in datePairs:
+    if i > 0:
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_MainContent_MainContent_btnBack"]').send_keys(Keys.RETURN)
+        
+    driver.find_element_by_xpath('//*[@id="ctl00_ctl00_MainContent_MainContent_txtStartDate"]').send_keys(datePair[0])
+    driver.find_element_by_xpath('//*[@id="ctl00_ctl00_MainContent_MainContent_txtEndDate"]').send_keys(datePair[1])
+    
+    time.sleep(1)
+    driver.find_element_by_xpath('//*[@id="ctl00_ctl00_MainContent_MainContent_btnSubmit"]').send_keys(Keys.RETURN)
+    pdb.set_trace()
+    time.sleep(1)
+    driver.find_element_by_xpath('//*[@id="ctl00_ctl00_MainContent_MainContent_lnkExport"]').send_keys(Keys.RETURN)
+    i = 1
 
 time.sleep(1)
 driver.find_element_by_xpath('//*[@id="aspnetForm"]/a').send_keys(Keys.RETURN)
